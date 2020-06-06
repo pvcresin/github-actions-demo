92a70019113288e8ecb8852c131d1f25ead37e8f
pvcresin <pvcresin0730@gmail.com>
2020-06-06 13:30:51 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+  },
+};
